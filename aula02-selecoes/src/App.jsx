65c1dda589ec5d98a0a766d92a497251e59e1e7d
ANDRÉ LUIZ DE ASSIS GUIMARÃES <andre_l_guimaraes@estudante.sesisenai.org.br>
2025-05-30 22:07:33 -0300

    Atualização Aula02-Seleção

--- aula02-selecoes/src/App.jsx
+++ aula02-selecoes/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [inputMarujos, setInputMarujos] = useState("");
+  const [inputEventos, setInputEventos] = useState("");
+  const [viagem, setViagem] = useState("");
+
+  function verificar() {
+
+    if (inputMarujos >= 10 && inputEventos >= 1) {
+      setViagem("SIM");
+    } else {
+      setViagem("NÃO");
+    }
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <h2>Ex. 9 - Capitão Ganso</h2>
+        Marujos:
+        <input
+          type="text"
+          value={inputMarujos}
+          onChange={(e) => setInputMarujos(e.target.value)}
+        />
+
+        Eventos:
+        <input
+          type="text"
+          value={inputEventos}
+          onChange={(e) => setInputEventos(e.target.value)}
+        />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={verificar}>Verificar</button>
+      <p >Viagem: {viagem}</p>
     </>
   )
 }
